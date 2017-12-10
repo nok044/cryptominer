276786759271900dbc8bfa3f8fd9e7adf55b9e72
Suched Chaisiricharoenkul <suched@cupcodeteam.com>
2017-12-10 14:07:23 +0700

    improve message

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ setInterval(function(){
                     latest = currentLatest;
                     for(var i = 0;i<observerList.length;i++) {
                         var userId = observerList[i];
+                        console.log('Send to '+userId)
                         str = (state === 'up' ? '\uDBC0\uDC5C ขึ้นละจ้า' : '\uDBC0\uDC7E ลงแล้วๆ')+' '+change.toFixed(2)+'% BTC Latest: '+currentLatest.toFixed(2)+' High: '+max.toFixed(2)+' Low:'+min.toFixed(2)+' Avg: '+avg.toFixed(2);
                         bot.push(userId, {
                             type: 'text',
